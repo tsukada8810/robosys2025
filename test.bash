ccd25edae2b30e79cadf459c0161f999773cc195
Hayato Tsukada <szhongtain85@gmail.com>
2025-11-06 12:31:13 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,18 +2,16 @@
 # SPDX-FileCopyrigtText: 2025 Hayato Tsukada
 # SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+        echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-
-out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./plus)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit $res
