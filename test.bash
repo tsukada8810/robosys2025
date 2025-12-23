613be46a34a031f6f6c895f9fafd4b38ce3c8ad2
Hayato Tsukada <szhongtain85@gmail.com>
2025-12-23 17:27:22 +0900

    fix a commnad

--- test.bash
+++ test.bash
@@ -1,20 +1,17 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrigtText: 2025 Hayato Tsukada
+# SPDX-FileCopyrightText: 2025 Hayato Tsukada
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-        echo ${1}行目が違うよ
-	res=1
-}
+out=$(echo "1 2 3 4" | ./robosyshw1)
+[ "$?" = 0 ] || exit 1
+[ "${out}" = "" ] && exit 1
 
-res=0
+out=$(echo "1 1 1 1" | ./robosyshw1 2>&1)
+[ "$?" = 1 ] || exit 1
+[ "${out}" = "" ] && exit 1
 
-out=$(echo 1 2 3 4 | ./robosyshw1 2>&1)
-[ "${out}" = "((1+2)+3)+4 = 10" ] || ng "$LINENO"
+out=$(./robosyshw1 1 2 3 4)
+[ "$?" = 0 ] || exit 1
 
-out=$(echo 1 1 1 1 | ./robosyshw1 2>&1)
-[ "${out}" = "no answer" ] || ng "$LINENO"
-
-[ "$res" = 0 ] && echo OK
-exit $res
+exit 0
 
